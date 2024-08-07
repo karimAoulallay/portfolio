34ab3e623e916680800a26571a06120d0ba33194
Abdelkarim Aoulallay <aoulallayabdelkarim@gmail.com>
2024-08-07 12:13:23 +0100

    Update hero image

--- src/components/HeroRightSide.jsx
+++ src/components/HeroRightSide.jsx
@@ -3,7 +3,7 @@ const HeroRightSide = () => {
     <div className="order-1 sm:order-2">
       <div className="image-container">
         <img
-          src="./public/images/no-bg-profile.png"
+          src="./images/no-bg-profile.png"
           alt="my profile image"
           className="max-w-[150px] md:max-w-[200px] lg:max-w-[250px] xl:max-w-[300px]"
         />
